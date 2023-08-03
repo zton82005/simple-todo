ff1581d0c0b28ffce8263871e625d3ab3845dc07
zton82005 <zton82005@gmail.com>
2023-08-04 01:05:00 +0800

    v2

--- app/page.js
+++ app/page.js
@@ -1,66 +1,135 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Image from "next/image";
 
 export default function Home() {
-	const [plan, setPlan] = useState("");
-	const [displayPlan, setDisplayPlan] = useState("");
-	const [yourName, setYourName] = useState("");
+	const [newItem, setNewItem] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [isEditing, setIsEditing] = useState(false);
+	const [editItemId, setEditItemId] = useState(null);
 
-	const handleInputNameChange = (event) => {
-		// Update the 'plan' state with the user's input from the input field
-		setYourName(event.target.value);
-	};
-	const handleInputChange = (e) => {
-		// Update the 'plan' state with the user's input from the input field
-		setPlan(e.target.value);
-	};
+	const inputRef = useRef(null);
 
-	const handleButtonClick = () => {
-		// Set the 'displayPlan' state with the 'plan' value when the button is clicked
-		if (plan === "") {
-			setDisplayPlan("Nothing");
-		} else {
-			setDisplayPlan(plan);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (newItem.trim() === "") {
+			alert("Please enter a task before adding or editing.");
+			return;
 		}
+
+		if (isEditing) {
+			// If editing, update the existing todo item
+			setTodos((prevTodos) =>
+				prevTodos.map((todo) =>
+					todo.id === editItemId ? { ...todo, title: newItem } : todo
+				)
+			);
+			setIsEditing(false);
+			setEditItemId(null);
+		} else {
+			// If not editing, add a new todo item
+			setTodos((prevTodos) => [
+				...prevTodos,
+				{ id: crypto.randomUUID(), title: newItem, completed: false }, // Initialize completed as false for new items
+			]);
+		}
+
+		setNewItem(""); // Clear the input field after adding or editing
 	};
+
+	const handleEdit = (id, title) => {
+		setIsEditing(true);
+		setEditItemId(id);
+		setNewItem(title);
+	};
+
+	const handleDelete = (id) => {
+		setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+	};
+
+	const handleCheckboxChange = (id) => {
+		setTodos((prevTodos) =>
+			prevTodos.map((todo) =>
+				todo.id === id ? { ...todo, completed: !todo.completed } : todo
+			)
+		);
+	};
+
+	useEffect(() => {
+		if (isEditing) {
+			inputRef.current.focus();
+		}
+	}, [isEditing]);
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center p-24 gap-10">
-			<Image
-				src="/test-img.jpg"
-				width={300}
-				height={200}
-				alt="computer"
-				className="rounded-md"
-			/>
-			<input
-				className="p-2 rounded-md text-black" // Add 'text-black' class to change the text color
-				type="text"
-				placeholder="Enter your name"
-				value={yourName}
-				onChange={handleInputNameChange}
-			/>
-			<input
-				className="p-2 rounded-md text-black" // Add 'text-black' class to change the text color
-				type="text"
-				placeholder="Enter text here"
-				value={plan}
-				onChange={handleInputChange}
-			/>
-			<button
-				className="p-2 ml-3 bg-slate-500 rounded-md hover:bg-slate-700"
-				onClick={handleButtonClick}
+		<div className="flex flex-col items-center p-6 md:p-24 gap-10">
+			<h1 className="text-2xl">Today's Tasks</h1>
+			<form
+				onSubmit={handleSubmit}
+				className="w-full md:w-1/3 flex flex-col sm:flex-row"
 			>
-				Display
-			</button>
+				<label htmlFor="item"></label>
+				<input
+					ref={inputRef}
+					className="p-2 rounded-lg sm:rounded-tl-lg sm:rounded-bl-lg text-gray-500 flex-1 border border-gray-400"
+					type="text"
+					placeholder="Enter a task"
+					value={newItem}
+					onChange={(e) => setNewItem(e.target.value)}
+					id="item"
+				/>
+				<button
+					className="py-2 px-3 rounded-lg sm:rounded-tr-lg sm:rounded-br-lg bg-yellow-400 text-black mt-2 sm:mt-0 sm:ml-1"
+					type="submit"
+				>
+					Add
+				</button>
+			</form>
+			<h2 className="text-xl">To Do's:</h2>
+			<div className="w-full md:w-1/3">
+				<ul className="border border-gray-400 rounded-lg divide-y divide-gray-400">
+					{todos.map((todo) => {
+						const isBeingEdited = isEditing && todo.id === editItemId;
 
-			<p className="mt-10 text-center">
-				Hello <span className="text-red-500">{yourName}</span>, <br />
-				Your plan for today:{" "}
-				{displayPlan && <span className="text-blue-300">{displayPlan}</span>}
-			</p>
-		</main>
+						return (
+							<li key={todo.id} className="flex items-center p-3">
+								<label className="flex items-center space-x-3 flex-1">
+									<input
+										className="text-white"
+										type="checkbox"
+										checked={todo.completed}
+										onChange={() => handleCheckboxChange(todo.id)}
+									/>
+									<span
+										className={`flex-1 ${
+											todo.completed ? "line-through text-gray-600" : ""
+										}`}
+									>
+										{todo.title}
+									</span>
+								</label>
+
+								<button
+									className="p-3 rounded-lg border border-yellow-400 items-center"
+									onClick={() => handleEdit(todo.id, todo.title)}
+									disabled={isBeingEdited}
+								>
+									<Image src="/edit.png" alt="edit" width={12} height={12} />
+								</button>
+								<button
+									className="p-3 rounded-lg border border-yellow-400 items-center ml-1"
+									onClick={() => handleDelete(todo.id)}
+									disabled={isBeingEdited}
+								>
+									<Image src="/trash.png" alt="delete" width={12} height={12} />
+								</button>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</div>
 	);
 }
